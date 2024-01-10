92516eaec0519d1f146949c1a35d0b600f41b0ee
o0th <o0th@pm.me>
2024-01-10 20:52:36 +0100

    feat: more splitting

---
+++ plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "github/copilot.vim",
+  event = "VeryLazy",
+  autoStart = true,
+  config = function()
+    vim.g.copilot_assume_mapped = true
+    vim.api.nvim_set_keymap("i", "<S-CR>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  end
+}
